080bcb888abb5f94ae81e3408194b35ba3e0dc2b
rachitasingh1 <rachitasingh243@gmail.com>
2024-04-17 20:48:53 +0530

    adding advice for service layer

--- src/main/java/com/example/aoplogging/aop/SpacemanAspect.java
+++ src/main/java/com/example/aoplogging/aop/SpacemanAspect.java
@@ -1,6 +1,7 @@
 package com.example.aoplogging.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -21,4 +22,20 @@ public class SpacemanAspect {
     public void beforeAdvice(JoinPoint joinPoint){
         System.out.println("Request to " +joinPoint.getSignature() + "started at "+ new Date());
     }
+
+    @After(value = "execution(* com.example.aoplogging.controller.SpacemanController.*(..))")
+    public void afterAdvice(JoinPoint joinPoint){
+        System.out.println("Request to " +joinPoint.getSignature() + "ended at "+ new Date());
+    }
+
+    @Before(value = "execution(* com.example.aoplogging.service.SpacemanService.*(..))")
+    public void beforeAdviceForService(JoinPoint joinPoint){
+        System.out.println("Request to service " +joinPoint.getSignature() + "started at "+ new Date());
+    }
+
+    @After(value = "execution(* com.example.aoplogging.service.SpacemanService.*(..))")
+    public void afterAdviceForService(JoinPoint joinPoint){
+        System.out.println("Request to service " +joinPoint.getSignature() + "ended at "+ new Date());
+    }
+
 }
