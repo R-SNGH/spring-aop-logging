495df764e45e125ff29ea7c854d75fbe725c58d9
rachitasingh1 <rachitasingh243@gmail.com>
2024-04-17 22:32:28 +0530

    working with @Around Advice

--- src/main/java/com/example/aoplogging/aop/SpacemanAspect.java
+++ src/main/java/com/example/aoplogging/aop/SpacemanAspect.java
@@ -2,7 +2,9 @@ package com.example.aoplogging.aop;
 
 import com.example.aoplogging.entities.Spaceman;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -47,5 +49,61 @@ public class SpacemanAspect {
     public void afterThrowingAdviceForService(JoinPoint joinPoint, Exception exception){
         System.out.println("Business logic to add Spaceman threw an exception:  " +exception.getMessage());
     }
+
+    //Around is the MOST POWERFUL Advice
+    // version 1 : basics
+//    @Around(value = "execution(* com.example.aoplogging.service.SpacemanService.addSpaceman(..))")
+//    public void aroundAdviceForService(ProceedingJoinPoint joinPoint){
+//        System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman started at: "+ new Date());
+//        try{
+//            //if we don't place proceed here, the actual method won't execute at all
+//            joinPoint.proceed();
+//        }catch(Throwable e){
+//            System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman failed terribly! "+ e.getMessage());
+//        }
+//        System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman ended at: "+ new Date());
+//    }
+
+    //Around version2 : returning something useful to Client
+//    @Around(value = "execution(* com.example.aoplogging.service.SpacemanService.addSpaceman(..))")
+//    public Spaceman aroundAdviceForService(ProceedingJoinPoint joinPoint){
+//        System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman started at: "+ new Date());
+//        try{
+//            Spaceman spaceman = (Spaceman) joinPoint.proceed();
+//            return spaceman;
+//        }catch(Throwable e){
+//            System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman failed terribly! "+ e.getMessage());
+//        }
+//        System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman ended at: "+ new Date());
+//        return null;
+//    }
+
+    //Around version 3: intercepting input arguments
+    @Around(value = "execution(* com.example.aoplogging.service.SpacemanService.addSpaceman(..))")
+    public Spaceman aroundAdviceForService(ProceedingJoinPoint joinPoint){
+        System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman started at: "+ new Date());
+        try{
+            Spaceman[] spacemen = new Spaceman[1];
+            Spaceman dummySpaceman = new Spaceman();
+            dummySpaceman.setName("Random Name Assigned");
+            spacemen[0]=dummySpaceman;
+            Spaceman spaceman = (Spaceman) joinPoint.proceed(spacemen);
+            return spaceman;
+        }catch(Throwable e){
+            System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman failed terribly! "+ e.getMessage());
+        }
+        System.out.println("Inside Around advice in Aspect: Business Logic to save Spaceman ended at: "+ new Date());
+        return null;
+    }
+
+    /*
+    Notes:
+    1) without joinPoint.proceed(); the actual method is not executed
+    2) the return type of this advice is the response to client; with void -> zero response; test on postman
+    3) we can write joinPoint.proceed(); multiple times to execute the method multiple times
+    4) WE can catch exception here and do our logging
+    5) We can stop method execution by not writing joinPoint.proceed(); and then return something that we wish to return to Client
+    6) We can transform the input as well here.
+    */
 
 }
